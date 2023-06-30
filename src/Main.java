85cad1a900eeb81253a8669877f1cd895fb7260b
TheComicMystery <81803055+TheComicMystery@users.noreply.github.com>
2023-06-30 10:41:41 +0300

    aaaaa

--- src/Main.java
+++ src/Main.java
@@ -8,64 +8,72 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         crud.ReadInfo();
 
+        label:
         while (true) {
             System.out.print("> ");
             String input = scanner.nextLine().toLowerCase();
 
-            if (input.equals("help")) {
-                System.out.println("'ShowAll' - see all tasks");
-                System.out.println("'Add' - create task");
-                System.out.println("'Read' - see task");
-                System.out.println("'Update' - update task");
-                System.out.println("'Delete' - remove task");
-                System.out.println("'Find' - search for tasks");
-                System.out.println("'Sort' - sort tasks");
-                System.out.println("'SaveInfo' - save data to the file");
-                System.out.println("'Exit' - quit");
-            } else if (input.equals("showall")) {
-                String result = crud.ShowAll();
-                System.out.println(result);
-            } else if (input.equals("add")) {
-                TodoListItemDTO item = new TodoListItemDTO();
-                System.out.println("Enter name of the task:");
-                item.Name = scanner.nextLine();
-                System.out.println("Enter status of the task:");
-                item.Status = scanner.nextLine();
-                crud.Create(item);
-            } else if (input.equals("read")) {
-                System.out.println("Enter Id of the task:");
-                int id = Integer.parseInt(scanner.nextLine());
-                String result = crud.Read(id);
-                System.out.println(result);
-            } else if (input.equals("update")) {
-                TodoListItemDTO item = new TodoListItemDTO();
-                System.out.println("Enter Id of the task:");
-                item.Id = Integer.parseInt(scanner.nextLine());
-                System.out.println("Enter name of the task:");
-                item.Name = scanner.nextLine();
-                System.out.println("Enter status of the task:");
-                item.Status = scanner.nextLine();
-                crud.Update(item);
-            } else if (input.equals("delete")) {
-                System.out.println("Enter Id of the task:");
-                int id = Integer.parseInt(scanner.nextLine());
-                crud.Delete(id);
-            } else if (input.equals("find")) {
-                System.out.println("Enter pattern:");
-                String pattern = scanner.nextLine();
-                String result = crud.Find(pattern);
-                System.out.println(result);
-            } else if (input.equals("sort")) {
-                crud.Sort();
-                String result = crud.ShowAll();
-                System.out.println(result);
-            } else if (input.equals("exit")) {
+            switch (input) {
+                case "help" -> {
+                    System.out.println("'ShowAll' - see all tasks");
+                    System.out.println("'Add' - create task");
+                    System.out.println("'Read' - see task");
+                    System.out.println("'Update' - update task");
+                    System.out.println("'Delete' - remove task");
+                    System.out.println("'Find' - search for tasks");
+                    System.out.println("'Sort' - sort tasks");
+                    System.out.println("'Exit' - quit");
+                }
+                case "showall" -> {
+                    String result = crud.ShowAll();
+                    System.out.println(result);
+                }
+                case "add" -> {
+                    TodoListItemDTO item = new TodoListItemDTO();
+                    System.out.println("Enter name of the task:");
+                    item.Name = scanner.nextLine();
+                    System.out.println("Enter status of the task:");
+                    item.Status = scanner.nextLine();
+                    crud.Create(item);
+                }
+                case "read" -> {
+                    System.out.println("Enter Id of the task:");
+                    int id = Integer.parseInt(scanner.nextLine());
+                    String result = crud.Read(id);
+                    System.out.println(result);
+                }
+                case "update" -> {
+                    TodoListItemDTO item = new TodoListItemDTO();
+                    System.out.println("Enter Id of the task:");
+                    item.Id = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter name of the task:");
+                    item.Name = scanner.nextLine();
+                    System.out.println("Enter status of the task:");
+                    item.Status = scanner.nextLine();
+                    crud.Update(item);
+                }
+                case "delete" -> {
+                    System.out.println("Enter Id of the task:");
+                    int id = Integer.parseInt(scanner.nextLine());
+                    crud.Delete(id);
+                }
+                case "find" -> {
+                    System.out.println("Enter pattern:");
+                    String pattern = scanner.nextLine();
+                    String result = crud.Find(pattern);
+                    System.out.println(result);
+                }
+                case "sort" -> {
+                    crud.Sort();
+                    String result = crud.ShowAll();
+                    System.out.println(result);
+                }
+                case "exit" -> {
                     String result = crud.Serialize();
                     crud.SaveInfo(result);
-                    break;
+                    break label;
                 }
-              else {
-                System.out.println("This command is not supported");
+                default -> System.out.println("This command is not supported");
             }
         }
     }
